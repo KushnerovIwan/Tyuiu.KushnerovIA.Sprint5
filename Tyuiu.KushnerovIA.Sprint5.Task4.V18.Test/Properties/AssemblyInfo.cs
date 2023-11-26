c44da1260c71f363d59ddb327d9ba5db14e6fe1a
kia45 <kia45@IKUSHNEROV>
2023-11-26 10:06:02 +0300

    Task4

---
+++ Tyuiu.KushnerovIA.Sprint5.Task4.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KushnerovIA.Sprint5.Task4.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.KushnerovIA.Sprint5.Task4.V18.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3fad6feb-308e-488c-b9be-14e277cae0c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
