e19b944d43b9a935a313ba73aeb7a0925c4fe474
kia45 <kia45@IKUSHNEROV>
2023-11-26 19:12:19 +0300

    Task7

---
+++ Tyuiu.KushnerovIA.Sprint5.Task7.V16/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KushnerovIA.Sprint5.Task7.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
